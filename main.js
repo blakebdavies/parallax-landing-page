c9312c61d1bd0bd751243f4c8d2378d1e17c4d54
Blake <81705490+blakebdavies@users.noreply.github.com>
2022-10-08 09:45:36 +0100

    created and styled hero section and added initial content

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Vite!</h1>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
